0b6409875e06772c8b3a331943ec7fb3d86cbb5e
Sviatoslav Bessarb <132812188+Saanvalat@users.noreply.github.com>
2023-10-26 19:51:15 +0300

    Update AplicationConstants.java

--- src/main/java/settings/bot/AplicationConstants.java
+++ src/main/java/settings/bot/AplicationConstants.java
@@ -1,8 +1,7 @@
 package settings.bot;
 
 public class AplicationConstants extends TelegramBot{
-    public static final String BOT_TOKEN = "6888449176:AAFoGMCJZLZzoGJFDhrwAisJT3_xxX1Ivs8";
-    public static final String BOT_NAME = "ExchangeCurrencyTGbot";
+
 
 
 
