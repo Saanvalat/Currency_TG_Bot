49d5a687afc7a707832bad176edc26b97f56dbc8
mac <s2000b00@ukr.net>
2023-10-27 00:05:19 +0300

    add file for constants

--- src/main/java/settings/bot/AplicationConstants.java
+++ src/main/java/settings/bot/AplicationConstants.java
@@ -1,1 +1,42 @@
+package settings.bot;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.module.Configuration;
+import org.json.JSONObject;
+
+
+
+
+
+public class AplicationConstants {
+
+
+    private JSONObject jsonObject;
+
+    public AplicationConstants() {
+        try {
+            JSONTokener tokener = new JSONTokener(new FileInputStream("config.json"));
+            jsonObject = new JSONObject(tokener);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getBotToken() {
+        return jsonObject.getString("BOT_TOKEN");
+    }
+
+    public String getBotName() {
+        return jsonObject.getString("BOT_NAME");
+    }
+}
+
+
+
+
+
+
 
